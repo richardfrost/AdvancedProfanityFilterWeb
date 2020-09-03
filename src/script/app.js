1f55f4d7c3f14f026c4414b3b86d35ff7f5cad52
Richard Frost <richardfrost@users.noreply.github.com>
2020-09-03 14:32:00 -0600

    :loud_sound: Add more logging

--- src/script/app.js
+++ src/script/app.js
@@ -14,11 +14,13 @@ export function processFile(res, file, cfg, genSummary = true) {
   const tmpDownload = encodeURI(`download/${fileOutput}`);
 
   try {
+    console.log(`Processing '${fileName}'...`);
     let result;
     if (!genSummary) { res.attachment(streamOutput); }
 
     switch(ext.toLowerCase()) {
       case '.docx':
+        console.log(`Treating '${fileName}' as an docx file`);
         result = filter.cleanDocx(file);
         if (result) {
           if (genSummary) {
@@ -29,26 +31,32 @@ export function processFile(res, file, cfg, genSummary = true) {
             res.send(result.toBuffer());
           }
         } else {
+          console.warn(`Nothing to filter for '${fileName}'`);
           res.render('pages/error', { error: 'Nothing was filtered' });
         }
         break;
       case '.epub':
+        console.log(`Treating '${fileName}' as an epub file`);
         result = filter.cleanEpub(file);
         if (result) {
           if (genSummary) {
+            console.log(`Generating summary for '${fileName}'`);
             result.writeZip(tmpFile); // TODO: Error handling
             let data = {summary: filter.summary, downloadHref: tmpDownload};
             res.render('pages/summary', data);
           } else {
+            console.log(`Sending filtered version of '${fileName}' (${tmpDownload})`);
             res.send(result.toBuffer());
           }
         } else {
+          console.warn(`Nothing to filter for '${fileName}'`);
           res.render('pages/error', { error: 'Nothing was filtered' });
         }
         break;
       case '.md':
       case '.srt':
       case '.txt':
+        console.log(`Treating '${fileName}' as a plain-text file`);
         result = filter.cleanText(file);
         if (result) {
           if (genSummary) {
@@ -64,10 +72,12 @@ export function processFile(res, file, cfg, genSummary = true) {
             res.send(result);
           }
         } else {
+          console.warn(`Nothing to filter for '${fileName}'`);
           res.render('pages/error', { error: 'Nothing was filtered' });
         }
         break;
       default:
+        console.log(`Treating '${fileName}' as other (textract) file`);
         result = filter.cleanOther(file);
         if (result) {
           if (genSummary) {
@@ -76,6 +86,7 @@ export function processFile(res, file, cfg, genSummary = true) {
             res.send(result);
           }
         } else {
+          console.warn(`Nothing to filter for '${fileName}'`);
           res.render('pages/error', { error: 'Nothing was filtered' });
         }
     }
